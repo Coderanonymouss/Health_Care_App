c00ae537a5b3624635c08c21a97565ad2dc4bb81
dinia <coder25012024@gmail.com>
2025-05-21 03:00:09 +0500

    add new test

--- app/src/androidTest/java/com/ensias/healthcareapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ensias/healthcareapp/ExampleInstrumentedTest.java
@@ -10,18 +10,15 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+        // Получение контекста приложения
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
+        // Проверка, что имя пакета совпадает с ожидаемым
         assertEquals("com.ensias.healthcareapp", appContext.getPackageName());
     }
 }
+
